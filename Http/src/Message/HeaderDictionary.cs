e7443def865a536ce81c6b5a6c7e202a29bef34b
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2024-06-12 21:15:31 -0400

    Create HeaderDictionary

---
+++ Http/src/Message/HeaderDictionary.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+
+namespace Http.Message;
+
+public class HeaderDictionary : IEnumerable<KeyValuePair<string, string>>
+{
+    private Dictionary<string, string> items = new();
+
+    public HeaderDictionary(Dictionary<string, string> items)
+    {
+        this.items = items;
+    }
+
+    public void Add(string key, string value)
+    {
+        items.Add(key, value);
+    }
+
+    public bool ContainsKey(string key)
+    {
+        return items.ContainsKey(key);
+    }
+
+    public string? GetValue(string key)
+    {
+        if (!ContainsKey(key))
+        {
+            return null;
+        }
+
+        return items[key];
+    }
+
+    public IEnumerator<KeyValuePair<string, string>> GetEnumerator()
+    {
+        return items.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
